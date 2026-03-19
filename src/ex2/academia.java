33313cfd940746f02d711f4eb80718f4f06f069c
joaovitor-2209 <vitorcruzrocha@gmail.com>
2026-03-19 09:41:30 -0300

    Criando metodos

--- src/ex2/academia.java
+++ src/ex2/academia.java
@@ -8,4 +8,38 @@ public class academia {
     String descanco;
     String carga;
     String dificuldade;
+
+    //Metodo => acoes
+    //Escrevo im trecho de codigo, esse metodo pode ser usado varias vezes
+    //public => metodo publico
+    //void => é um tipo (vazio) de retorno / metodo sem retorno
+    //Nome de metodo = começa com letra minuscul, usa padrao camelcase, nao pode conter carac. especais
+
+    public void exibir(){
+        System.out.println("Exercicio: " + exercicio);
+        System.out.println("Descrição: " + resumo);
+        System.out.println("Repetições: " + repeticoes);
+        System.out.println("Séries: " + serie);
+        System.out.println("Tempo de descanço: " + descanco);
+        System.out.println("Dificuldade: " + dificuldade);
+        System.out.println("===============================");
+
+    }
+
+    // Metodo com retorno
+    // public tipo_retorno nomeMetodo(){}
+    // Sempre devemos retornar o tipo solicitado usando a palavra return valor / variavel
+
+    public String exibirComRetorno(){
+        String mensagem = "\nDescrição do exercício\n" + exercicio + "\n" +
+                resumo   + "\n" +
+                "Séries: " + serie + "\n" +
+                "Repetições: " + repeticoes + "\n" +
+                "Carga: "  + carga + " kg"  + "\n" +
+                "Dificuldade: " + dificuldade +"\n" +
+                "=============================================";
+
+        return mensagem;
+   }
 }
+
--- src/ex2/exercicio.java
+++ src/ex2/exercicio.java
@@ -1,6 +1,6 @@
 package ex2;
 
-import ex1.bolo;
+import ex2.academia;
 
 public class exercicio {
     public static void main(String[] args) {
@@ -9,17 +9,29 @@ public class exercicio {
         novoex.resumo = "É um exercício que fortalece e\n" +
                 "melhora a definição dos músculos\n" +
                 "do peitoral...";
-        novoex.repeticoes = Integer.parseInt("15");
-        novoex.serie = Integer.parseInt("3");
+        novoex.repeticoes = 15;
+        novoex.serie = 3;
         novoex.descanco = "2 min";
-        novoex.carga = "70 kg";
+        novoex.carga = "34 kg";
         novoex.dificuldade = "Dificil";
+        novoex.exibir();
+        novoex.exibirComRetorno();
+        System.out.println(novoex.exibirComRetorno());
 
-        System.out.println(novoex.exercicio);
-        System.out.println(novoex.resumo);
-        System.out.println(novoex.repeticoes);
-        System.out.println(novoex.serie);
-        System.out.println(novoex.descanco);
-        System.out.println(novoex.dificuldade);
+        academia ex2 = new academia();
+        ex2.exercicio = "Puxada frontal";
+        ex2.resumo = "É um exercício que fortalece e\n" +
+                "melhora a definição dos músculos\n" +
+                "da costa...";
+        ex2.repeticoes = 12;
+        ex2.serie = 3;
+        ex2.descanco = "2 min";
+        ex2.carga = "50 kg";
+        ex2.dificuldade = "Dificil";
+        ex2.exibir();
+        ex2.exibirComRetorno();
+        System.out.println(ex2.exibirComRetorno());
+
+
     }
 }
